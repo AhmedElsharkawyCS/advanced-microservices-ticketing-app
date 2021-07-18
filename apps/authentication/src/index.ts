c67cdf1a1b2c01e9c57fc8a654fbdfe952c77944
AhmedElsharkawyCS <ahmedelsharkawy.fcih@gmail.com>
2021-07-18 02:26:30 +0200

    Create service and deployment for auth service

--- apps/authentication/src/index.ts
+++ apps/authentication/src/index.ts
@@ -2,7 +2,9 @@ import express, { json } from "express";
 const app = express();
 
 app.use(json());
-
+app.get("/api/users/me", (req, res, next) => {
+  res.send("Hi there!");
+});
 app.listen(3000, () => {
   console.log("Listing on port: 3000");
 });
